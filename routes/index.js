9b631dad5bd3c03cb488c6a0c9b72302d3c9ac2c
Viktor Kaderabek <kaderabek.viktor@seznam.cz>
2015-05-26 15:58:17 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function (req, res, next) {
+  var data = {
+    message: "Hello World!",
+    innerMsg: {
+      innerMessage: "Hola gente!",
+      evenDeeper: {
+        evenDeeper: "Hola europeanos!"
+      }
+    }
+  };
+  res.send(data);
+});
+
+module.exports = router;
